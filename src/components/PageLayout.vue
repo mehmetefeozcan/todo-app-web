f3392fc04815d04ac849222042733273344056be
Mehmet Efe Özcan <93211996+MehmetEfe0zcan@users.noreply.github.com>
2022-10-17 21:25:50 +0300

    wip

---
+++ src/components/PageLayout.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="relative flex">
+    <div class="flex flex-col bg-gray-200 text-black h-screen w-48">
+      <div>drawer</div>
+    </div>
+
+    <div class="flex flex-row text-black h-16 w-screen relative bg-gray-200">
+      header
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      items: [{ title: "Dashboard" }, { title: "Account" }, { title: "Admin" }],
+      drawer: false,
+    };
+  },
+  methods: {
+    drawerOpen() {
+      this.drawer = !this.drawer;
+    },
+  },
+};
+</script>
+
+<style></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,14 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
-</template>
+<template><PageLayout /></template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import PageLayout from "../components/PageLayout.vue";
 export default {
   name: "HomeView",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      open: true,
+    };
   },
+  components: { PageLayout },
 };
 </script>
